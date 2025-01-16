f15c515a2ec2d06de452ae7bd7e87a8c54f26d70
Andrew Jones <aej11@outlook.com>
2025-01-16 07:56:56 -0500

    don't show personal desc box for guest

--- components/PersonalizedDescription.tsx
+++ components/PersonalizedDescription.tsx
@@ -7,6 +7,9 @@ export default async function PersonalizedDescription({
   productId: number;
 }) {
   const user = await getSignedInUser();
+  if(!user) {
+    return null;
+  }
   const customerSegments = user ? user.segment_ids : [];
   const personalizedProduct = await getProductWithPersonalizedDescription(
     productId,
