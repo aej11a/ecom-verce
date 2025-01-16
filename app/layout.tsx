9459f4ebf71c42ea1eb90bc4f66f07f2ec49df39
Andrew Jones <aej11@outlook.com>
2025-01-16 00:05:45 -0500

    Update meta desc

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 
 export const metadata = {
   title: 'Glass & Co.',
-  description: 'Your one-stop e-commerce solution',
+  description: 'Your one-stop glassware shop',
 }
 
 export default function RootLayout({
