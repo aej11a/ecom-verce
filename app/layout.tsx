7b7466d01253fd416568fc2de5c591024ab76404
Andrew Jones <aej11@hotmail.com>
2025-01-16 14:12:27 -0500

    Speed insights and analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,24 @@
-import './globals.css'
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import "./globals.css";
 
 export const metadata = {
-  title: 'Glass & Co.',
-  description: 'Your one-stop glassware shop',
-}
+  title: "Glass & Co.",
+  description: "Your one-stop glassware shop",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+        <SpeedInsights />
+      </body>
     </html>
-  )
+  );
 }
-
